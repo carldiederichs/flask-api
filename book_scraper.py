49a13338798fff8bc090e15c7119ef64406b7757
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-01 20:24:15 +0100

    added star rating and converted to integers

--- book_scraper.py
+++ book_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 BASE_URL = 'https://books.toscrape.com/'
 
@@ -8,12 +9,28 @@ page = requests.get(BASE_URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 bookshelf = soup.findAll("li",
-                         {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+                      {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
 
 book_title_collection = []
 for books in bookshelf: 
     book_title = books.h3.a['title']
-    book_price = books.div.p['price_color']
-    book_title_collection.extend([book_title], [book_price])
-print(book_title_collection)
-    
+    book_prices = soup.findAll('p', {'class':'price_color'})
+    book_price = book_prices[0].text.strip()
+    book_ratings = soup.findAll('p', {'class':'star-rating'})
+    for book_rating in book_ratings:
+        book_rating_stripped= book_rating.get('class')[1]
+        if book_rating_stripped == "One":
+            book_rating_stripped = 1
+        elif book_rating_stripped == "Two":
+            book_rating_stripped = 2
+        elif book_rating_stripped == "Three":
+            book_rating_stripped = 3
+        elif book_rating_stripped == "Four":
+            book_rating_stripped = 4
+        elif book_rating_stripped == "Five":
+            book_rating_stripped = 5
+    case = {'title':book_title, 'price':book_price, 'rating': book_rating_stripped}
+    book_title_collection.append(case)
+json_string = json.dumps(book_title_collection)
+print(json_string)
+
---
+++ sudocode.py
@@ -0,0 +1,15 @@
+# Build API that connects endpoints to data from books.toscrape.com
+
+# Build basic API functionality
+    # Prepare flask endpoints
+# Scrape required data
+    # Decide which datapoints to scrape
+    # Access different data points
+    # Build for loop around scraped data
+        # Build for loop for one page 
+        # Build for loop to loop through all pages
+# Connect API to scraper
+    # Put scraped data into json format
+    # Write scraper functionality into API functions
+    
+    
