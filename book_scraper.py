f591f0990da99c9180674c2aff62dd5fc4f5e88d
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-02 23:46:05 +0100

    corrected star rating

--- book_scraper.py
+++ book_scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from word2number import w2n
+
 
 BASE_URL = 'https://books.toscrape.com/'
 
@@ -17,20 +19,9 @@ for books in bookshelf:
     book_prices = soup.findAll('p', {'class':'price_color'})
     book_price = book_prices[0].text.strip()
     book_ratings = soup.findAll('p', {'class':'star-rating'})
-    for book_rating in book_ratings:
-        book_rating_stripped= book_rating.get('class')[1]
-        if book_rating_stripped == "One":
-            book_rating_stripped = 1
-        elif book_rating_stripped == "Two":
-            book_rating_stripped = 2
-        elif book_rating_stripped == "Three":
-            book_rating_stripped = 3
-        elif book_rating_stripped == "Four":
-            book_rating_stripped = 4
-        elif book_rating_stripped == "Five":
-            book_rating_stripped = 5
-    case = {'title':book_title, 'price':book_price, 'rating': book_rating_stripped}
+    for book_rating in (book_ratings):
+        book_ratings_stripped = book_rating.get('class')[i]
+    case = {'title':book_title, 'price':book_price, 'rating': book_ratings_stripped}
     book_title_collection.append(case)
 json_string = json.dumps(book_title_collection)
 print(json_string)
-
