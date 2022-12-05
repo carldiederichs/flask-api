e65b867183b5a27d738d5ce4f67d9f2d3fd776ec
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-05 21:54:13 +0100

    merged files and connected book_info endpoint to retrieve all book information

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from flask import Flask, jsonify, request, send_file
 from flask import request
+from bs4 import BeautifulSoup
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route('/hello', methods = ['GET', 'POST'])
-def welcome():
-    return jsonify({'name':'Carl',
-                    'message':'WELCOME TO MY API'}, 200)
-    
-@app.route('/numbers')
-def print_numbers():
-    return jsonify(list(range(5)))
+BASE_URL = 'https://books.toscrape.com/'
+
+page = requests.get(BASE_URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+books = soup.find_all('article')
+book_information_scraped = []
+
+@app.route('/book_info', methods = ['GET'])
+def book_info():
+    for book in books: 
+        book_title = book.h3.a['title']
+        book_price = book.find('p', class_='price_color').text
+        book_rating = book.find('p', class_='star-rating').attrs.get('class')[1]
+        case = {'title':book_title, 'price':book_price, 'rating': book_rating}
+        book_information_scraped.append(case)
+    return json.dumps(book_information_scraped)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
-    
-data = request.data
-print(data)
