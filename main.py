daaae5f9e14f04f7f58cce885c9ce5e87c7c5f5e
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-08 21:18:04 +0100

    added functionality to scrape all 50 pages

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 from flask import Flask
 from flask import Flask, jsonify, request, send_file
 from flask import request
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bfs
 import json
-import requests
+import requests as rq
 from word2number import w2n
-import eel
 import webbrowser
 from threading import Timer
+import time
 
 app = Flask(__name__)
 
-BASE_URL = 'https://books.toscrape.com/'
-
-page = requests.get(BASE_URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-books = soup.find_all('article')
 book_information_scraped = []
 single_book_information = []
 
 @app.route('/book_info', methods = ['GET'])
 def book_info():
-    for book in books: 
-        book_title = book.h3.a['title']
-        book_price = book.find('p', class_='price_color').text
-        book_rating = book.find('p', class_='star-rating').attrs.get('class')[1]
-        book_rating_int = w2n.word_to_num(book_rating)
-        case = {'title':book_title, 'price':book_price, 'rating': book_rating_int}
-        book_information_scraped.append(case)
+    page = 1
+    while page <= 50:
+        url = "https://books.toscrape.com/catalogue/page-{}.html".format(page)
+        response = rq.get(url)
+        page_content = bfs(response.content, 'html.parser')
+        books = page_content.find_all('article')
+        print(f'Now scraping page {page}')
+        
+        for book in books: 
+            book_title = book.h3.a['title']
+            book_price = book.find('p', class_='price_color').text
+            book_rating = book.find('p', class_='star-rating').attrs.get('class')[1]
+            book_rating_int = w2n.word_to_num(book_rating)
+            case = {'title':book_title, 'price':book_price, 'rating': book_rating_int}
+            book_information_scraped.append(case)
+        page += 1
+        time.sleep(2)
     return json.dumps(book_information_scraped)
 
 @app.route('/book_info_single/<title>', methods = ['GET', 'POST'])
 def book_info_single(title):
-    for i, book in enumerate(book_information_scraped):
+    for book_details, book in enumerate(book_information_scraped):
         if book['title'] == title:
-            return book_information_scraped[i]
+            return book_information_scraped[book_details]
     return "Invalid Title"
 
 def open_browser():
