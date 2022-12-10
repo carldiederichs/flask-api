116e2cd20b9b11a8280b461107c6b4f617b9fd47
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-10 23:35:40 +0100

    started scraping subpages

--- main.py
+++ main.py
@@ -26,17 +26,15 @@ def book_info():
         print(f'Now scraping page {page}')
         
         for book in books: 
-            url_individual = f"{BASE_URL}/{book.find('a')['href']}"
-            book_source = f"{BASE_URL}/{book.find('img')['src']}"
-            x = book_source.rfind('/')
-            urllib.request.urlretrieve(book_source, f'thumbs/{book_source[x+1:]}')
-            breakpoint()
+            # url_individual = f"{BASE_URL}/{book.find('a')['href']}"
+            # book_source = f"{BASE_URL}/{book.find('img')['src']}"
+            # x = book_source.rfind('/')
+            # urllib.request.urlretrieve(book_source, f'thumbs/{book_source[x+1:]}')
             book_title = book.h3.a['title']
             book_price = book.find('p', class_='price_color').text
             book_rating = book.find('p', class_='star-rating').attrs.get('class')[1]
             book_rating_int = w2n.word_to_num(book_rating)
-            case = {'title':book_title, 'price':book_price, 'rating': book_rating_int, 
-                    'source': book_source, 'url':url_individual}
+            case = {'title':book_title, 'price':book_price, 'rating': book_rating_int}
             book_information_scraped.append(case)
         page += 1
     return json.dumps(book_information_scraped)
