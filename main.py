f617aa22fdf72762228812c147f741652dd4dff7
Carl Diederichs <carl-diederichs@t-online.de>
2022-12-10 23:34:44 +0100

    started scraping subpages

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask import Flask, jsonify, request, send_file
 from flask import request
 from bs4 import BeautifulSoup as bfs
@@ -7,17 +6,19 @@ import requests as rq
 from word2number import w2n
 import webbrowser
 from threading import Timer
-import time
+import urllib.request
 
 app = Flask(__name__)
 
 book_information_scraped = []
 single_book_information = []
 
+BASE_URL = "https://books.toscrape.com"
+
 @app.route('/book_info', methods = ['GET'])
 def book_info():
     page = 1
-    while page <= 50:
+    while page <= 5:
         url = "https://books.toscrape.com/catalogue/page-{}.html".format(page)
         response = rq.get(url)
         page_content = bfs(response.content, 'html.parser')
@@ -25,14 +26,19 @@ def book_info():
         print(f'Now scraping page {page}')
         
         for book in books: 
+            url_individual = f"{BASE_URL}/{book.find('a')['href']}"
+            book_source = f"{BASE_URL}/{book.find('img')['src']}"
+            x = book_source.rfind('/')
+            urllib.request.urlretrieve(book_source, f'thumbs/{book_source[x+1:]}')
+            breakpoint()
             book_title = book.h3.a['title']
             book_price = book.find('p', class_='price_color').text
             book_rating = book.find('p', class_='star-rating').attrs.get('class')[1]
             book_rating_int = w2n.word_to_num(book_rating)
-            case = {'title':book_title, 'price':book_price, 'rating': book_rating_int}
+            case = {'title':book_title, 'price':book_price, 'rating': book_rating_int, 
+                    'source': book_source, 'url':url_individual}
             book_information_scraped.append(case)
         page += 1
-        time.sleep(2)
     return json.dumps(book_information_scraped)
 
 @app.route('/book_info_single/<title>', methods = ['GET', 'POST'])
